6d3a60d8f7d113fb5231b2394d87ef37ce1cb973
谭思齐 <tsq_kevin@buaa.edu.cn>
2021-08-30 14:17:43 +0800

    fix db_to_csv.py

--- db_to_csv.py
+++ db_to_csv.py
@@ -7,7 +7,7 @@ with open("results.csv", "w", encoding="utf-8") as f:
     robot_max_cnt = 10
     f.write("no,site_width,site_height,room_cnt,injury_cnt,departure_position,robot_type,robot_cnt,mode,room_visited,"
             "injury_rescued,returned,total_action_cnt,"
-            f"{','.join(('robot_{}_visits,robot_{}_rescues,robot_{}_collides'.format(i, i, i) for i in range(robot_max_cnt)))}")
+            f"{','.join(('robot_{}_visits,robot_{}_rescues,robot_{}_collides'.format(i, i, i) for i in range(robot_max_cnt)))}\n")
     for row in result:
         for col in row:
             print(col, end=",", file=f)
