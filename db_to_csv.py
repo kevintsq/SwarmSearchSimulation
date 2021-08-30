3460cb3c31970e097a5c99463619aa7514f2ae4c
谭思齐 <tsq_kevin@buaa.edu.cn>
2021-08-30 13:31:20 +0800

    fix db_to_csv.py

--- db_to_csv.py
+++ db_to_csv.py
@@ -1,8 +1,13 @@
 import sqlite3
+
 connection = sqlite3.connect('results.db')
 cursor = connection.cursor()
 result = cursor.execute("SELECT * FROM results")
 with open("results.csv", "w", encoding="utf-8") as f:
+    robot_max_cnt = 10
+    f.write("no,site_width,site_height,room_cnt,injury_cnt,departure_position,robot_type,robot_cnt,mode,room_visited,"
+            "injury_rescued,returned,total_action_cnt,"
+            f"{','.join(('robot_{}_visits,robot_{}_rescues,robot_{}_collides'.format(i, i, i) for i in range(robot_max_cnt)))}")
     for row in result:
         for col in row:
             print(col, end=",", file=f)
