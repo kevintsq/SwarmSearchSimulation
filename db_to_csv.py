4179ced36d6c4c777cbed73415db8c3a484940f9
谭思齐 <tsq_kevin@buaa.edu.cn>
2021-09-11 01:30:07 +0800

    fix stat bug

--- db_to_csv.py
+++ db_to_csv.py
@@ -3,7 +3,7 @@ import sqlite3
 filename = 'results_small_ljy'
 connection = sqlite3.connect(f'{filename}.db')
 cursor = connection.cursor()
-result = cursor.execute("SELECT * FROM results WHERE robot_cnt < 11 AND mode = 'Search' AND ('1000' = total_action_cnt OR room_cnt = CAST(room_visited AS INT))")
+result = cursor.execute("SELECT * FROM results WHERE robot_cnt < 11 AND mode = 'Search' AND (total_action_cnt = 1000 OR room_cnt = room_visited)")
 with open(f"{filename}_filtered.csv", "w", encoding="utf-8") as f:
     robot_max_cnt = 10
     f.write("no,site_width,site_height,room_cnt,injury_cnt,departure_position,robot_type,robot_cnt,mode,room_visited,"
